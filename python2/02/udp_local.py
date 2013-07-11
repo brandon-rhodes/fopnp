6be1a73dd1ba32dbb2d50552731a31148ea21ac2
Brandon Rhodes <brandon@rhodesmill.org>
2013-07-11 12:23:55 -0400

    Fix UDP error experienced by Windows users
    Windows does not support getsockname() on UDP sockets before they are
    first used and receive a port number.

--- python2/02/udp_local.py
+++ python2/02/udp_local.py
@@ -5,22 +5,21 @@
 import socket, sys
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-MAX = 65535
+BUFSIZE = 65535
 PORT = 1060
 
 if sys.argv[1:] == ['server']:
     s.bind(('127.0.0.1', PORT))
     print 'Listening at', s.getsockname()
     while True:
-        data, address = s.recvfrom(MAX)
+        data, address = s.recvfrom(BUFSIZE)
         print 'The client at', address, 'says', repr(data)
         s.sendto('Your data was %d bytes' % len(data), address)
 
 elif sys.argv[1:] == ['client']:
-    print 'Address before sending:', s.getsockname()
     s.sendto('This is my message', ('127.0.0.1', PORT))
     print 'Address after sending', s.getsockname()
-    data, address = s.recvfrom(MAX)  # overly promiscuous - see Chapter 2
+    data, address = s.recvfrom(BUFSIZE)  # overly promiscuous - see Chapter 2
     print 'The server', address, 'says', repr(data)
 
 else:
