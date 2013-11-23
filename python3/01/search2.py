7c1676f4d5289405e8dfad5281c5a0949ea98831
Brandon Rhodes <brandon@rhodesmill.org>
2013-11-23 13:50:04 -0500

    Switch search2.py example over to using "requests"

--- python3/01/search2.py
+++ python3/01/search2.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 # Foundations of Python Network Programming - Chapter 1 - search2.py
 
-import json
-from urllib.parse import urlencode
-from urllib.request import urlopen
+import requests
 
 def geocode(address):
-    params = {'address': address, 'sensor': 'false'}
-    base = 'http://maps.googleapis.com/maps/api/geocode/json?'
-    url = base + urlencode(params)
-    raw_reply = urlopen(url).read()
-    reply = json.loads(raw_reply.decode('utf-8'))
-    print(reply['results'][0]['geometry']['location'])
+    parameters = {'address': address, 'sensor': 'false'}
+    base = 'http://maps.googleapis.com/maps/api/geocode/json'
+    response = requests.get(base, params=parameters)
+    answer = response.json()
+    print(answer['results'][0]['geometry']['location'])
 
 if __name__ == '__main__':
     geocode('207 N. Defiance St, Archbold, OH')
