a9c1fae12c9050ed6e32a4de6511b1d7d26b8060
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-03 15:03:08 -0500

    Try rewriting mime_decode.py for new API
    Also, throw in some argparse for awesome.

--- py3/chapter12/mime_decode.py
+++ py3/chapter12/mime_decode.py
@@ -2,25 +2,35 @@
 # Foundations of Python Network Programming, Third Edition
 # https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter12/mime_decode.py
 
-import email, fileinput, sys
+import argparse, email.message, email.policy
 
-def save_parts(message, level=0, counter=1):
-    l = "|  " * level
-    if message.is_multipart():
-        print(l + "Found multipart:")
-        for item in message.get_payload():
-            counter = save_parts(item, level + 1, counter)
-    else:
-        filename = 'part{}.out'.format(counter)
-        print('Part {}:'.format(counter),
-              message.get('content-type', '-'),
-              message.get('content-disposition', '-'),
-              '=>', filename)
-        with open(filename, 'wb') as f:
-            f.write(message.get_payload(decode=1))
-        counter += 1
-    return counter
+def walk(message, level=0):
+    yield (level, message)
+    if message.get_content_maintype() == 'multipart':
+        for part in message.iter_parts():
+            yield from walk(part, level + 1)
+
+def save_parts(message):
+    counter = 0
+    for level, message in walk(message):
+        indent = '  ' * level
+        if message.get_content_maintype() == 'multipart':
+            print(counter, indent, message.get_content_type())
+        else:
+            counter += 1
+            filename = 'part{}.out'.format(counter)
+            print(counter, indent, message.get_content_type(),
+                  message.get('content-disposition', '-'), '=>', filename)
+            content = message.get_content()
+            mode = 'w' if isinstance(content, str) else 'wb'
+            with open(filename, mode) as f:
+                f.write(content)
+
+def main():
+    parser = argparse.ArgumentParser(description='Save message in part*.txt')
+    parser.add_argument('filename', help='Input email message filename')
+    with open(parser.parse_args().filename) as f:
+        save_parts(email.message_from_file(f, policy=email.policy.default))
 
 if __name__ == '__main__':
-    message = email.message_from_string(''.join(fileinput.input()))
-    save_parts(message)
+    main()
