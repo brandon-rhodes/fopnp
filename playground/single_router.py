7cabb5f690f3cae14b1d90416b6fbb6d2de6f6b2
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-05 13:07:19 -0500

    Give each pair of hosts its own subnet

--- playground/single_router.py
+++ playground/single_router.py
@@ -13,18 +13,25 @@ class RoutedTopo(Topo):
         Topo.__init__(self, **opts)
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
+        h1 = self.addHost('h1', ip=None)
+        h2 = self.addHost('h2', ip=None)
+        h3 = self.addHost('h3', ip=None)
         self.addLink(h1, s1)
         self.addLink(h2, s1)
         self.addLink(h2, s2)
         self.addLink(h3, s2)
+        # print self[h2]
+        # print dir(h2)
+        # h2.cmd('ip addr add 10.0.2.1/24 dev h2-eth1')
 
 def main():
     topo = RoutedTopo()
     net = Mininet(topo, controller=OVSController)
     net.start()
+    net['h1'].cmd('ip addr add 10.0.1.2/24 dev h1-eth0')
+    net['h2'].cmd('ip addr add 10.0.1.1/24 dev h2-eth0')
+    net['h2'].cmd('ip addr add 10.0.2.1/24 dev h2-eth1')
+    net['h3'].cmd('ip addr add 10.0.2.2/24 dev h3-eth0')
     print "Host connections:"
     dumpNodeConnections(net.hosts)
     net.terms += makeTerms(net.hosts, 'host')
