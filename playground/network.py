3e33ecc4358473183446c38fd4ff2cefca14f497
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-08 00:13:28 -0500

    Start building network in our diagram

--- playground/network.py
+++ playground/network.py
@@ -10,38 +10,88 @@ from mininet.util import dumpNodeConnections
 
 class RoutedTopo(Topo):
     def __init__(self, **opts):
-        """h1 <-> s1 <-> h2 <-> s2 <-> h3"""
         Topo.__init__(self, **opts)
+
+        isp = self.addHost('isp', ip='10.25.1.1/16')
+        modemA = self.addHost('modemA', ip='10.25.1.65/16')
+
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
-        h1 = self.addHost('h1', ip='10.0.1.2/24')
-        h2 = self.addHost('h2', ip='10.0.1.1/24')
-        h3 = self.addHost('h3', ip='10.0.2.2/24')
+
+        h1 = self.addHost('h1', ip='192.168.1.11/24')
+        h2 = self.addHost('h2', ip='192.168.1.12/24')
+        h3 = self.addHost('h3', ip='192.168.1.13/24')
+
+        h4 = self.addHost('h4', ip='192.168.1.11/24')
+
+        self.addLink(modemA, isp)
+        self.addLink(modemA, s1)
         self.addLink(h1, s1)
         self.addLink(h2, s1)
-        self.addLink(h2, s2)
-        self.addLink(h3, s2)
+        self.addLink(h3, s1)
+
+        self.addLink(h4, s2)
+
+
+        return
+        s3 = self.addSwitch('s3')
+
+        modemB = self.addHost('modemB', ip='10.50.1.66/16')
+
+        backbone = self.addHost('backbone', ip='10.1.1.1/8')
+
+        example = self.addHost('example.com', ip='10.130.1.1/24')
+
+        ftp = self.addHost('ftp', ip='10.130.1.2/24')
+        mail = self.addHost('mail', ip='10.130.1.3/24')
+        www = self.addHost('www', ip='10.130.1.4/24')
+
+        self.addLink(backbone, isp)
+        self.addLink(isp, modemA)
+        self.addLink(isp, modemB)
+        self.addLink(modemB, s2)
+
+
+
+        self.addLink(backbone, example)
+        self.addLink(example, s3)
+        self.addLink(ftp, s3, ftp)
+        self.addLink(mail, s3)
+        self.addLink(www, s3)
 
 def main(do_interactive):
+    hosts = 'h1', #'h2', 'h3', 'h4'
+    modems = 'modemA', #'modemB'
+    gateways = 'isp', 'backbone', 'example.com'
+
     topo = RoutedTopo()
     net = Mininet(topo, controller=OVSController)
     net.start()
-    net['h2'].cmd('ip addr add 10.0.2.1/24 dev h2-eth1')
-    net['h1'].cmd('route add default gw 10.0.1.1')
-    net['h3'].cmd('route add default gw 10.0.2.1')
-    net['h2'].cmd('echo 1 > /proc/sys/net/ipv4/ip_forward')
+
+    for host in hosts:
+        net[host].cmd('route add default gw 192.168.1.1')
+
+    for modem in modems:
+        net[modem].cmd('ip addr add 192.168.1.1/24 dev %s-eth1' % modem)
+        net[modem].cmd('route add default gw 10.25.1.1')
+        net[modem].cmd('echo 1 > /proc/sys/net/ipv4/ip_forward')
+
+    # for name in hosts + modems + gateways:
+    #     net[host].cmd('dnsmasq --interface=lo --no-dhcp-interface=lo'
+    #                   ' --no-daemon --no-resolv --no-hosts'
+    #                   ' --addn-hosts=/home/brandon/fopnp/playground/hosts &')
+
+    #net['isp'].cmd('ip
+
+    #net['h2'].cmd('echo 1 > /proc/sys/net/ipv4/ip_forward')
     print "Host connections:"
     dumpNodeConnections(net.hosts)
     if do_interactive:
-        hosts = ['h1', 'h2', 'h3']
-        for host in hosts:
-            net[host].cmd('dnsmasq --interface=lo --no-dhcp-interface=lo'
-                          ' --no-daemon --no-resolv --no-hosts'
-                          ' --addn-hosts=/home/brandon/fopnp/playground/hosts &')
-        net.terms += makeTerms(net.hosts, 'host')
+        hosts = [net['h1'], net['modemA']]
+        net.terms += makeTerms(hosts, 'host')  # net.hosts
         CLI(net)
-        for host in hosts:
-            net[host].cmd('kill %dnsmasq')
+        # for host in hosts:
+        #     host.cmd('kill %dnsmasq')
     else:
         net.pingAll()
     net.stop()
