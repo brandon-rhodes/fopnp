d5974f0abc37db068fb05307fbe126593f063a9a
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-25 18:12:52 -0400

    Write sanity-check script for URLs in comments

---
+++ py3/check.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#
+# Check listings for sanity
+
+import os
+
+base = 'https://github.com/brandon-rhodes/fopnp/blob/m/py3/'
+
+def main():
+    for dirpath, dirnames, filenames in os.walk('.'):
+        if dirpath == '.':
+            continue
+        if dirpath.startswith('./'):
+            dirpath = dirpath[2:]
+        for filename in filenames:
+            if not filename.endswith('.py'):
+                continue
+            path = os.path.join(dirpath, filename)
+            content = open(path).read()
+            url = base + path
+            if url + '\n' not in content:
+                print(path)
+
+if __name__ == '__main__':
+    main()
