d38c6693127b0668c21a980961d129bd1bc7b9f1
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-25 19:01:13 -0400

    Add "random.py" with seed(0) to stabilize output
    Also, set PYTHONDONTWRITEBYTECODE, just for fun.  Because I just
    discovered it in the man page.

--- py3/run_session.py
+++ py3/run_session.py
@@ -33,7 +33,8 @@ def main():
 
     shell_input = ''.join(commands).encode('ascii')
     env = {'LANG': 'en_US.UTF-8', 'PS1': banner + '\n' + '$ ',
-           'PATH': os.environ['PATH']}
+           'PATH': os.environ['PATH'], 'PYTHONPATH': '..',
+           'PYTHONDONTWRITEBYTECODE': '1'}
     p = Popen([
         '/bin/sh',
         '-i',  # interactive: print PS1 prompt before each line
@@ -43,9 +44,6 @@ def main():
     output = output.replace(b'$ \n', b'')
     output = re.sub(rb'Date: .*',
                     b'Date: Tue, 25 Mar 2014 17:14:01 -0400',
-                    output)
-    output = re.sub(rb'Message-ID: <[\d.]+@guinness>',
-                    b'Message-ID: <20140325211401.9307.43420@guinness>',
                     output)
     results.append(output.decode('utf-8'))
 
