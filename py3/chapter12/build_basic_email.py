95d197d2d4ad6bb109e4dd36219e0f260024aee1
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-26 11:27:59 -0400

    Have build_basic_email.py use plain email.Message

--- py3/chapter12/build_basic_email.py
+++ py3/chapter12/build_basic_email.py
@@ -2,7 +2,7 @@
 # Foundations of Python Network Programming, Third Edition
 # https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter12/build_basic_email.py
 
-from email.message import EmailMessage
+from email.message import Message
 
 text = """Hello,
 
@@ -11,12 +11,12 @@ This is a test message from Chapter 12.
  - Anonymous"""
 
 def main():
-    msg = EmailMessage()
-    msg['To'] = 'recipient@example.com'
-    msg['From'] = 'Test Sender <sender@example.com>'
-    msg['Subject'] = 'Test Message, Chapter 12'
-    msg.set_content(text)
-    print(msg.as_string())
+    message = Message()
+    message['To'] = 'recipient@example.com'
+    message['From'] = 'Test Sender <sender@example.com>'
+    message['Subject'] = 'Test Message, Chapter 12'
+    message.set_payload(text)
+    print(message.as_string())
 
 if __name__ == '__main__':
     main()
