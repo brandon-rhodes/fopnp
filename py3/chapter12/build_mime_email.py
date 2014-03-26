cd4ef0f8ad31e5ef52e5d3dc5ab9d67a3ff629a7
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-26 11:56:15 -0400

    Make "build_mime_email.py" symmetric with "basic"
    This way, both the basic-email and mime-email scripts look roughly the
    same and use the same variable names.

--- py3/chapter12/build_mime_email.py
+++ py3/chapter12/build_mime_email.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # Foundations of Python Network Programming, Third Edition
-# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter12/build_basic_email.py
+# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter12/build_fancier_email.py
 
-import email.message
+import email.message, email.utils
 
 text = """Hello,
 
@@ -11,11 +11,13 @@ This is a test message from Chapter 12.
  - Anonymous"""
 
 def main():
-    message = email.message.Message()
+    message = email.message.EmailMessage()
     message['To'] = 'recipient@example.com'
     message['From'] = 'Test Sender <sender@example.com>'
     message['Subject'] = 'Test Message, Chapter 12'
-    message.set_payload(text)
+    message['Date'] = email.utils.formatdate(localtime=True)
+    message['Message-ID'] = email.utils.make_msgid()
+    message.set_content(text)
     print(message.as_string())
 
 if __name__ == '__main__':
