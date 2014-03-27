a62261e2aef88faee19aa8d62bd0cdb3f27c7278
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-27 14:02:44 -0400

    Show how to display and iterate over MIME messages

---
+++ py3/chapter12/display_email.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# Foundations of Python Network Programming, Third Edition
+# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter12/display_email.py
+#
+# Someday this script will use message.iter_attachments() instead of
+# walking through them itself, but http://bugs.python.org/issue21079
+
+import argparse, email.policy, sys
+
+def main(binary_file):
+    default = email.policy.default
+    message = email.message_from_binary_file(binary_file, policy=default)
+    for header in ['From', 'To', 'Date', 'Subject']:
+        print(header + ':', message.get(header, '(none)'))
+    print()
+
+    try:
+        body = message.get_body(preferencelist=('plain', 'html'))
+    except KeyError:
+        print('<This message lacks a printable text or HTML body>')
+    else:
+        print(body.get_content())
+
+    for part in message.walk():
+        cd = part['Content-Disposition']
+        is_attachment = cd and cd.split(';')[0].lower() == 'attachment'
+        if not is_attachment:
+            continue
+        content = part.get_content()
+        print('* Attachment named {!r}: {} object of length {}'.format(
+            part.get_filename(), type(content).__name__, len(content)))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Parse and print an email')
+    parser.add_argument('filename', nargs='?', help='File containing an email')
+    args = parser.parse_args()
+    if args.filename is None:
+        main(sys.stdin.buffer)
+    else:
+        with open(args.filename, 'rb') as f:
+            main(f)
---
+++ py3/chapter12/display_structure.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# Foundations of Python Network Programming, Third Edition
+# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter12/display_structure.py
+#
+# Someday this script will use part.is_attachment instead of making the
+# decision itself, but http://bugs.python.org/issue21079
+
+import argparse, email.policy, sys
+
+def walk(part, prefix=''):
+    yield prefix, part
+    for i, subpart in enumerate(part.iter_parts()):
+        yield from walk(subpart, prefix + '.{}'.format(i))
+
+def main(binary_file):
+    default = email.policy.default
+    message = email.message_from_binary_file(binary_file, policy=default)
+    for prefix, part in walk(message):
+        line = '{} type={}'.format(prefix, part.get_content_type())
+        if part.get_content_maintype() != 'multipart':
+            content = part.get_content()
+            line += ' {} len={}'.format(type(content).__name__, len(content))
+            cd = part['Content-Disposition']
+            is_attachment = cd and cd.split(';')[0].lower() == 'attachment'
+            if is_attachment:
+                line += ' attachment'
+            filename = part.get_filename()
+            if filename is not None:
+                line += ' filename={!r}'.format(filename)
+        print(line)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Display MIME structure')
+    parser.add_argument('filename', nargs='?', help='File containing an email')
+    args = parser.parse_args()
+    if args.filename is None:
+        main(sys.stdin.buffer)
+    else:
+        with open(args.filename, 'rb') as f:
+            main(f)
