71a4e21049a8c42301bb52752590657e447d9e7f
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-27 19:23:50 -0400

    Add Date and Message-ID even to simplest example
    Why did my earlier edition of the book even show how to generate an
    email without these two fields?  What does saving those two lines of
    code even accomplish?

--- py3/chapter12/build_basic_email.py
+++ py3/chapter12/build_basic_email.py
@@ -2,7 +2,7 @@
 # Foundations of Python Network Programming, Third Edition
 # https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter12/build_basic_email.py
 
-import email.message, sys
+import email.message, email.utils, sys
 
 text = """Hello,
 This is a basic message from Chapter 12.
@@ -13,6 +13,8 @@ def main():
     message['To'] = 'recipient@example.com'
     message['From'] = 'Test Sender <sender@example.com>'
     message['Subject'] = 'Test Message, Chapter 12'
+    message['Date'] = email.utils.formatdate(localtime=True)
+    message['Message-ID'] = email.utils.make_msgid()
     message.set_content(text)
     sys.stdout.buffer.write(message.as_bytes())
 
