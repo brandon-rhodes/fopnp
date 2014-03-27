d04ea90f9b5e7e0ecc45bc7df0df61cb5a90c435
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-26 20:27:42 -0400

    Rebuild MIME examples with a single modern script

--- py3/chapter12/build_mime_email.py
+++ py3/chapter12/build_mime_email.py
@@ -1,24 +1,63 @@
 #!/usr/bin/env python3
 # Foundations of Python Network Programming, Third Edition
-# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter12/build_fancier_email.py
+# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter12/build_mime_email.py
 
-import email.message, email.utils
+import argparse, email.message, email.utils, mimetypes
 
-text = """Hello,
+plain = """Hello,\n\nThis is a test message from Chapter 12.\n\n - Anonymous"""
 
-This is a test message from Chapter 12.
+html = """<p>Hello,</p>
+<p>This is a <b>test message</b> from Chapter 12.</p>
+<p>This is the smallest possible blue GIF:</p>
+<img src="cid:{}" height="80" width="80">
+<p>- <i>Anonymous</i></p>"""
 
- - Anonymous"""
+# Tiny example GIF from http://www.perlmonks.org/?node_id=7974
+blue_dot = (b'GIF89a1010\x900000\xff000,000010100\x02\x02\x0410;'
+            .replace(b'0', b'\x00').replace(b'1', b'\x01'))
 
-def main():
+def main(args):
+    cid = email.utils.make_msgid()  # per RFC 2392, must be globally unique!
+
     message = email.message.EmailMessage()
     message['To'] = 'recipient@example.com'
     message['From'] = 'Test Sender <sender@example.com>'
     message['Subject'] = 'Test Message, Chapter 12'
     message['Date'] = email.utils.formatdate(localtime=True)
     message['Message-ID'] = email.utils.make_msgid()
-    message.set_content(text)
+
+    if not args.h and not args.i:
+        message.set_content(plain)
+    elif not args.h:
+        message.set_content(plain)
+        message.add_attachment(blue_dot, 'image', 'gif', disposition='inline',
+                               filename='blue-dot.gif')
+    else:
+        message.set_content(html.format(cid.strip('<>')), subtype='html')
+        if args.i:
+            message.add_related(blue_dot, 'image', 'gif', cid=cid,
+                                filename='blue-dot.gif')
+        message.add_alternative(plain)
+
+    for filename in args.filename:
+        mime_type, encoding = mimetypes.guess_type(filename)
+        if encoding or (mime_type is None):
+            mime_type = 'application/octet-stream'
+        main, sub = mime_type.split('/')
+        if main == 'text':
+            with open(filename, encoding='utf-8') as f:
+                text = f.read()
+            message.add_attachment(text, sub, filename=filename)
+        else:
+            with open(filename, 'rb') as f:
+                data = f.read()
+            message.add_attachment(data, main, sub, filename=filename)
+
     print(message.as_string())
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(add_help=False, description='Build email')
+    parser.add_argument('-h', action='store_true', help='Include HTML part')
+    parser.add_argument('-i', action='store_true', help='Include GIF image')
+    parser.add_argument('filename', nargs='*', help='Attachment filename')
+    main(parser.parse_args())
