9c7dd320eb389f09927aa11526ac9b4c1fef7003
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-31 17:00:50 -0400

    Add "dovecot" to the packages needed under Ubuntu

--- playground/install.sh
+++ playground/install.sh
@@ -3,4 +3,6 @@
 # Install the dependencies needed to run our Mininet and all of its
 # services on Ubuntu.
 
-sudo apt-get install mininet dnsmasq
+sudo apt-get install dnsmasq dovecot-imapd dovecot-pop3d mininet
+stop dovecot
+mv /etc/dovecot/dovecot.conf /etc/dovecot/dovecot-OFF.conf
