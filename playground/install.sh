799fd5722d3257d6271ada7015c5a98bdf6b499c
Brandon Rhodes <brandon@rhodesmill.org>
2014-04-21 09:00:46 -0400

    Switch "install.sh" to not calling "sudo"
    It seems a better design to have users add the "sudo" part, so that they
    understand the permissions that the script is running with, and know
    where the "password" prompt is coming from.

--- playground/install.sh
+++ playground/install.sh
@@ -3,6 +3,13 @@
 # Install the dependencies needed to run our Mininet and all of its
 # services on Ubuntu.
 
-sudo apt-get install dnsmasq dovecot-imapd dovecot-pop3d mininet
-sudo stop dovecot
-sudo mv /etc/dovecot/dovecot.conf /etc/dovecot/dovecot-OFF.conf
+if [ "$(id -u)" != "0" ]
+then
+    echo 'Error: "install.sh" must be run as root; try: sudo ./install.sh'
+    exit 2
+fi
+
+set -e
+apt-get install dnsmasq dovecot-imapd dovecot-pop3d mininet
+stop dovecot
+mv /etc/dovecot/dovecot.conf /etc/dovecot/dovecot-OFF.conf
