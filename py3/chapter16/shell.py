600b5028040e4c6aca02e3dd31427e70f2aaae99
Brandon Rhodes <brandon@rhodesmill.org>
2014-04-23 12:03:20 -0400

    Update "shell.py" so code lives inside "main()"

--- py3/chapter16/shell.py
+++ py3/chapter16/shell.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 # Foundations of Python Network Programming, Third Edition
 # https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter16/shell.py
-# A simple shell, so you can try running commands in the absence of
-# any special characters (except for whitespace, used for splitting).
+# A simple shell, so you can try running commands at a prompt where no
+# characters are special (except that whitespace separates arguments).
 
 import subprocess
 
-while True:
-    args = input('] ').split()
-    if not args:
-        pass
-    elif args == ['exit']:
-        break
-    elif args[0] == 'show':
-        print("Arguments:", args[1:])
-    else:
-        subprocess.call(args)
+def main():
+    while True:
+        args = input('] ').strip().split()
+        if not args:
+            pass
+        elif args == ['exit']:
+            break
+        elif args[0] == 'show':
+            print("Arguments:", args[1:])
+        else:
+            try:
+                subprocess.call(args)
+            except Exception as e:
+                print(e)
+
+if __name__ == '__main__':
+    main()
