800575301608998e60ae89f80d99adbe55e9c7dd
Brandon Rhodes <brandon@rhodesmill.org>
2014-04-25 10:25:22 -0400

    Switch back to an sshd per Docker instance

--- playground/base/startup.sh
+++ playground/base/startup.sh
@@ -5,4 +5,4 @@
 # exists in the networking playground will append additional commands to
 # this startup script using RUN commands in their Dockerfiles.
 
-trap "exec /bin/bash" EXIT
+/etc/init.d/ssh start
