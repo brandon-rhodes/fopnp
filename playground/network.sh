1c18638d88f4ff8ede235c88ad5eebbc5aa756a8
Brandon Rhodes <brandon@rhodesmill.org>
2014-04-25 21:57:27 -0400

    Get the three server machines up and running

--- playground/network.sh
+++ playground/network.sh
@@ -70,24 +70,39 @@ start_bridge playcom
 # main network namespace where we can add it to the bridge.
 
 sudo ip link add backbone-isp type veth peer name isp-backbone
+
 sudo ip link add modemA-isp type veth peer name isp-modemA
 sudo ip link add modemB-isp type veth peer name isp-modemB
 sudo ip link add modemA-eth1 type veth peer name modemA-peer
 sudo ip link add modemB-eth1 type veth peer name modemB-peer
+
+sudo ip link add backbone-dotcom type veth peer name dotcom-backbone
+sudo ip link add dotcom-eth1 type veth peer name dotcom-peer
+sudo ip link add ftp-eth0 type veth peer name ftp-peer
+sudo ip link add mail-eth0 type veth peer name mail-peer
+sudo ip link add www-eth0 type veth peer name www-peer
 
 # Take all of the interfaces that are destined to stay out here, outside
 # of any particular container, and connect them to bridges.
 
 sudo brctl addif playhome modemA-eth1
 sudo brctl addif playhome2 modemB-eth1
+sudo brctl addif playcom dotcom-eth1
+sudo brctl addif playcom ftp-eth0
+sudo brctl addif playcom mail-eth0
+sudo brctl addif playcom www-eth0
 
 # Take all of the other interfaces and throw them over the wall into
 # particular containers as we start them running with Docker.
 
-start_host fopnp/dns backbone eth0=backbone-isp
+start_host fopnp/dns backbone eth0=backbone-isp eth1=backbone-dotcom
 start_host fopnp/base isp eth0=isp-backbone eth1=isp-modemA eth2=isp-modemB
 start_host fopnp/base modemA eth0=modemA-isp eth1=modemA-peer
 start_host fopnp/base modemB eth0=modemB-isp eth1=modemB-peer
+start_host fopnp/base examplecom eth0=dotcom-backbone eth1=dotcom-peer
+start_host fopnp/base ftp eth0=ftp-peer
+start_host fopnp/base mail eth0=mail-peer
+start_host fopnp/base www eth0=www-peer
 
 # Okay!  We now have running containers with network interfaces
 # successfully installed inside, but none of them know how to talk to
@@ -115,6 +130,25 @@ do
     sudo ip netns exec $modem iptables --table nat \
         --append POSTROUTING --out-interface eth0 -j MASQUERADE
 done
+
+sudo ip netns exec $backbone ip addr add 10.1.1.1/32 dev eth1
+sudo ip netns exec $backbone ip route add 10.130.1.1/32 dev eth1
+sudo ip netns exec $backbone ip route add 10.130.1.0/24 via 10.130.1.1
+
+sudo ip netns exec $examplecom ip addr add 10.130.1.1/32 dev eth0
+sudo ip netns exec $examplecom ip route add 10.1.1.1/32 dev eth0
+sudo ip netns exec $examplecom ip route add default via 10.1.1.1
+sudo ip netns exec $examplecom ip addr add 10.130.1.1/24 dev eth1
+
+sudo ip netns exec $ftp ip addr add 10.130.1.2/24 dev eth0
+sudo ip netns exec $ftp ip route add default via 10.130.1.1
+
+sudo ip netns exec $mail ip addr add 10.130.1.3/24 dev eth0
+sudo ip netns exec $mail ip route add default via 10.130.1.1
+
+sudo ip netns exec $www ip addr add 10.130.1.4/24 dev eth0
+sudo ip netns exec $www ip route add default via 10.130.1.1
+
 
 echo Ready
 read
