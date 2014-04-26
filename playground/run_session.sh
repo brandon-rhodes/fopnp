5bcfd6f2c68e02a5fb2bb016f0b919c537f9d799
Brandon Rhodes <brandon@rhodesmill.org>
2014-04-26 12:54:22 -0400

    Run session inside of Docker, not Mininet

--- playground/run_session.sh
+++ playground/run_session.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 cd "$(dirname ${BASH_SOURCE[0]})"
-sudo -u brandon \
-    PATH=/home/brandon/usr/3.4/bin:/bin:/usr/bin \
-    python3 ../py3/run_session.py
+./play.sh h9 /usr/bin/python3 /py3/run_session.py
