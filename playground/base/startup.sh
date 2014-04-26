e9cc76823d30b3d8e492d31b69d16d5fad33aa5d
Brandon Rhodes <brandon@rhodesmill.org>
2014-04-26 11:55:45 -0400

    Start up syslog on all containers

--- playground/base/startup.sh
+++ playground/base/startup.sh
@@ -5,4 +5,5 @@
 # exists in the networking playground will append additional commands to
 # this startup script using RUN commands in their Dockerfiles.
 
+/etc/init.d/rsyslog start
 /etc/init.d/ssh start
