67e0c34ce316ca65f46c23024b155832bb0bca68
Brandon Rhodes <brandon@rhodesmill.org>
2014-04-26 11:21:31 -0400

    Avoid rebuild when requirements.txt doesn't change

--- playground/build.sh
+++ playground/build.sh
@@ -8,7 +8,13 @@ then
     ssh-keygen -f base/id_rsa -N ''
 fi
 
-cat ../py3/requirements.txt | grep -v pygeo > ./base/requirements.txt
+cat ../py3/requirements.txt | grep -v pygeo > ./base/requirements.txt.new
+if diff ./base/requirements.txt ./base/requirements.txt.new >/dev/null
+then
+    rm ./base/requirements.txt.new
+else
+    mv ./base/requirements.txt.new ./base/requirements.txt
+fi
 
 docker build -t fopnp/base base
 docker build -t fopnp/dns dns
