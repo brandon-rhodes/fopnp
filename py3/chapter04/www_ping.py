ad39224d3b26567298ced00bb7e098738bf3adc2
Brandon Rhodes <brandon@rhodesmill.org>
2014-06-02 17:50:14 -0400

    Rework www_ping.py for new edition

--- py3/chapter04/www_ping.py
+++ py3/chapter04/www_ping.py
@@ -3,30 +3,30 @@
 # https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter04/www_ping.py
 # Find the WWW service of an arbitrary host using getaddrinfo().
 
-import socket, sys
+import argparse, socket, sys
 
-if len(sys.argv) != 2:
-    print('usage: www_ping.py <hostname_or_ip>', file=sys.stderr)
-    sys.exit(2)
+def connect_to(hostname_or_ip):
+    try:
+        infolist = socket.getaddrinfo(
+            hostname_or_ip, 'www', 0, socket.SOCK_STREAM, 0,
+            socket.AI_ADDRCONFIG | socket.AI_V4MAPPED | socket.AI_CANONNAME,
+            )
+    except socket.gaierror as e:
+        print('Name service failure:', e.args[1])
+        sys.exit(1)
 
-hostname_or_ip = sys.argv[1]
+    info = infolist[0]  # per standard recommendation, try the first one
+    socket_args = info[0:3]
+    address = info[4]
+    s = socket.socket(*socket_args)
+    try:
+        s.connect(address)
+    except socket.error as e:
+        print('Network failure:', e.args[1])
+    else:
+        print('Success: host', info[3], 'is listening on port 80')
 
-try:
-    infolist = socket.getaddrinfo(
-        hostname_or_ip, 'www', 0, socket.SOCK_STREAM, 0,
-        socket.AI_ADDRCONFIG | socket.AI_V4MAPPED | socket.AI_CANONNAME,
-        )
-except socket.gaierror as e:
-    print('Name service failure:', e.args[1])
-    sys.exit(1)
-
-info = infolist[0]  # per standard recommendation, try the first one
-socket_args = info[0:3]
-address = info[4]
-s = socket.socket(*socket_args)
-try:
-    s.connect(address)
-except socket.error as e:
-    print('Network failure:', e.args[1])
-else:
-    print('Success: host', info[3], 'is listening on port 80')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Try connecting to port 80')
+    parser.add_argument('hostname', help='hostname that you want to contact')
+    connect_to(parser.parse_args().hostname)
