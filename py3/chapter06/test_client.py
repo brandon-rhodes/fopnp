0c26677e9349cda0692538545fe4ae2071ef1d35
Brandon Rhodes <brandon@rhodesmill.org>
2014-06-24 17:42:14 -0400

    Beautify output of test_client.py
    And, remove grungy comments full of experiments and redundant code.

--- py3/chapter06/test_client.py
+++ py3/chapter06/test_client.py
@@ -3,73 +3,74 @@
 # https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter06/tls_example.py
 # Building an encrypted TLS connection
 
-import argparse, os, socket, ssl, sys
+import argparse, socket, ssl, textwrap
 import ctypes
-from ctypes import POINTER, Structure, c_char_p, c_ulong, c_void_p, cast
 from pprint import pprint
 
-def client(hostname, port, debug=False):
-    #ctypes.cdll.LoadLibrary(ssl._ssl.__file__)
-    # print(ssl.__file__)
-    # print(ssl._ssl.__file__)
-    # print('VERSION:', lib.SSL_version())
-
+def open_ssl_connection(hostname, port, debug=False):
+    say('Server we want to talk to', hostname)
     raw_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     raw_sock.connect((hostname, port))
-    ca_certs_path = os.path.join(os.path.dirname(__file__), 'certfiles.crt')
-    sock = ssl.wrap_socket(raw_sock,
-                           ssl_version=ssl.PROTOCOL_SSLv23,
-                           #ssl_version=ssl.PROTOCOL_TLSv1_2,
-                           cert_reqs=ssl.CERT_REQUIRED,
-                           ca_certs=ca_certs_path)
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    ssl_sock = context.wrap_socket(raw_sock)
 
-    print(ssl.create_default_context().check_hostname)
-    print(ssl.create_default_context().protocol)
+    cert = ssl_sock.getpeercert()
+    names = [name for names in cert['subject'] for (kind, name) in names
+             if kind == 'commonName']
+    if 'subjectAltName' in cert:
+        names.extend(name for s, name in cert['subjectAltName'] if s == 'DNS')
 
-    context = sock.context
-    print(context.check_hostname)
-    print(context.cert_store_stats())
-    #print(context.get_ca_certs()[1])  # TODO: full list?
-
-    # Does the certificate that the server proffered *really* match the
-    # hostname to which we are trying to connect?  We need to check.
-
-    #print(sock.compression())
-    pprint(sock.getpeercert())
+    say('Name(s) on its server certificate', *names)
+    say('Whether name(s) match the hostname', test(cert, hostname))
+    for category, count in sorted(context.cert_store_stats().items()):
+        say('Certificates loaded of type {}'.format(category), count)
 
     try:
-        ssl.match_hostname(sock.getpeercert(), hostname)
-    except ssl.CertificateError as ce:
-        print('Certificate error:', str(ce))
-        sys.exit(1)
-
-    print('Cipher:', sock.cipher())
-
-    try:
-        print('PROTOCOL VERSION:', repr(SSL_get_version(sock)))
+        protocol_version = SSL_get_version(ssl_sock)
     except Exception:
         if debug:
             raise
+    else:
+        say('Protocol version negotiated', protocol_version)
 
-    # From here on, our `sock` works like a normal socket.  We can,
-    # for example, make an impromptu HTTP call.
+    cipher, version, bits = ssl_sock.cipher()
+    compression = ssl_sock.compression()
 
-    sock.sendall(b'GET / HTTP/1.0\r\n\r\n')
-    result = sock.makefile().read()  # quick way to read until EOF
-    sock.close()
-    print('The document https://%s/ is %d bytes long' % (hostname, len(result)))
+    say('Cipher chosen for this connection', cipher)
+    say('Cipher defined in TLS version', version)
+    say('Cipher key has this many bits', bits)
+    say('Compression algorithm in use', compression or 'none')
 
-class PySSLSocket(Structure):
+    return cert
+
+def say(title, *words):
+    text = title.ljust(36, '.') + ' ' + ' '.join(str(w) for w in words)
+    print(textwrap.fill(text, subsequent_indent=' ' * 8,
+                        break_long_words=False, break_on_hyphens=False))
+
+def test(cert, hostname):
+    """Convert the result of match_hostname() (an exception) to a string."""
+    try:
+        ssl.match_hostname(cert, hostname)
+    except ssl.CertificateError as e:
+        return str(e)
+    else:
+        return 'Yes'
+
+class PySSLSocket(ctypes.Structure):
     """The first few fields of a PySSLSocket (see Python's Modules/_ssl.c)."""
-    _fields_ = [('ob_refcnt', c_ulong), ('ob_type', c_void_p),
-                ('Socket', c_void_p), ('ssl', c_void_p)]
+
+    _fields_ = [('ob_refcnt', ctypes.c_ulong), ('ob_type', ctypes.c_void_p),
+                ('Socket', ctypes.c_void_p), ('ssl', ctypes.c_void_p)]
 
 def SSL_get_version(ssl_sock):
     """Reach behind the scenes for a socket's TLS protocol version."""
+
     lib = ctypes.CDLL(ssl._ssl.__file__)
     lib.SSL_get_version.restype = ctypes.c_char_p
     address = id(ssl_sock._sslobj)
-    struct = cast(address, POINTER(PySSLSocket)).contents
+    struct = ctypes.cast(address, ctypes.POINTER(PySSLSocket)).contents
     version_bytestring = lib.SSL_get_version(struct.ssl)
     return version_bytestring.decode('ascii')
 
@@ -78,7 +79,13 @@ if __name__ == '__main__':
     parser.add_argument('host', help='remote host to which to connect')
     parser.add_argument('-p', metavar='PORT', type=int, default=443,
                         help='TCP port (default 443)')
+    parser.add_argument('-c', action='store_true', default=False,
+                        help='pretty-print certificate information')
     parser.add_argument('-d', action='store_true', default=False,
                         help='debug mode: do not hide "ctypes" exceptions')
     args = parser.parse_args()
-    client(args.host, args.p, args.d)
+    print()
+    cert = open_ssl_connection(args.host, args.p, args.d)
+    print()
+    if args.c:
+        pprint(cert)
