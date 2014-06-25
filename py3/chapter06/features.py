b9fa9905da0aa769cdba21a553e82f1e61e1ffe3
Brandon Rhodes <brandon@rhodesmill.org>
2014-06-24 21:02:33 -0400

    Add a library to scan "ssl" module for constants

---
+++ py3/chapter06/features.py
@@ -0,0 +1,32 @@
+"""Introspect Python's SSL library and list its features and options."""
+
+try:
+    import ssl
+except ImportError:
+    ssl = None
+
+def main():
+    if ssl is None:
+        print('This Python is not compiled with SSL support')
+        return
+    names = dir(ssl)
+    print()
+    display(names, ' protocol ', lambda s: s.startswith('PROTOCOL_'))
+    display(names, ' verify_mode ', lambda s: s.startswith('CERT_'))
+    display(names, ' verify_flags ', lambda s: s.startswith('VERIFY_'))
+    display(names, ' options ', lambda s: s.startswith('OP_'))
+    display(names, ' feature availability ', lambda s: s.startswith('HAS_'))
+
+def display(names, title, test):
+    items = [(fix(getattr(ssl, name)), name) for name in names if test(name)]
+    print(title.center(72, '-'))
+    for value, name in sorted(items):
+        print('{:27} {:10}  {:>32}'.format(name, value, bin(value)[2:]))
+    print()
+
+def fix(value):
+    """Turn negative 32-bit numbers into positive numbers."""
+    return (value + 2 ** 32) if (value < 0) else value
+
+if __name__ == '__main__':
+    main()
