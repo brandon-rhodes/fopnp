ac6974b02a3943fa00c0326d329695172adbc9b3
Brandon Rhodes <brandon@rhodesmill.org>
2014-06-25 17:23:58 -0400

    Add "simple_tls.py" showing best practices for TLS

---
+++ py3/chapter06/simple_tls.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# Foundations of Python Network Programming, Third Edition
+# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter06/test_tls.py
+# Simple TLS client and server using safe configuration defaults
+
+import argparse, socket, ssl
+
+def client(host, port, cafile=None):
+    purpose = ssl.Purpose.SERVER_AUTH
+    context = ssl.create_default_context(purpose, cafile=cafile)
+
+    raw_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    raw_sock.connect((host, port))
+    print('Connected to host {!r} and port {}'.format(host, port))
+    ssl_sock = context.wrap_socket(raw_sock, server_hostname=host)
+
+    ssl_sock.sendall('Simple is better than complex.'.encode('ascii'))
+    ssl_sock.close()
+
+def server(host, port, certfile, cafile=None):
+    purpose = ssl.Purpose.CLIENT_AUTH
+    context = ssl.create_default_context(purpose, cafile=cafile)
+    context.load_cert_chain(certfile)
+
+    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listener.bind((host, port))
+    listener.listen(1)
+    print('Listening at interface {!r} and port {}'.format(host, port))
+    raw_sock, address = listener.accept()
+    print('Connection from host {!r} and port {}'.format(*address))
+    ssl_sock = context.wrap_socket(raw_sock, server_side=True)
+
+    while True:
+        data = ssl_sock.recv(1024)
+        if not data:
+            break
+        print(data)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Safe TLS client and server')
+    parser.add_argument('host', help='hostname or IP address')
+    parser.add_argument('port', type=int, help='TCP port number')
+    parser.add_argument('-a', metavar='cafile', default=None,
+                        help='authority: path to CA certificate PEM file')
+    parser.add_argument('-s', metavar='certfile', default=None,
+                        help='run as server: path to server PEM file')
+    args = parser.parse_args()
+    if args.s:
+        server(args.host, args.port, args.s, args.a)
+    else:
+        client(args.host, args.port, args.a)
