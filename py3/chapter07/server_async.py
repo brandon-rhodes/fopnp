92f781c438954cd15f7db4106254e0ae6a48df04
Brandon Rhodes <brandon@rhodesmill.org>
2014-08-06 03:23:17 -0400

    Clean up old raw async server code

---
+++ py3/chapter07/server_async.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# Foundations of Python Network Programming, Third Edition
+# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter07/server_poll.py
+# Asynchronous I/O driven by the poll() system call.
+
+import select, zen_example
+
+def all_events_forever(poll_object):
+    while True:
+        for fd, event in poll_object.poll():
+            yield fd, event
+
+def serve(listener):
+    sockets = {listener.fileno(): listener}
+    addresses = {}
+    bytes_received = {}
+    bytes_to_send = {}
+
+    poll_object = select.poll()
+    poll_object.register(listener, select.POLLIN)
+
+    for fd, event in all_events_forever(poll_object):
+        sock = sockets[fd]
+
+        # Socket closed: remove it from our data structures.
+
+        if event & (select.POLLHUP | select.POLLERR | select.POLLNVAL):
+            address = addresses.pop(sock)
+            rb = bytes_received.pop(sock, b'')
+            sb = bytes_to_send.pop(sock, b'')
+            if rb:
+                print('Client {} sent {} but then closed'.format(address, rb))
+            elif sb:
+                print('Client {} closed before we sent {}'.format(address, sb))
+            else:
+                print('Client {} closed socket normally'.format(address))
+            poll_object.unregister(fd)
+            del sockets[fd]
+
+        # New socket: add it to our data structures.
+
+        elif sock is listener:
+            sock, address = sock.accept()
+            print('Accepted connection from {}'.format(address))
+            sock.setblocking(False)         # makes this whole scheme work
+            sockets[sock.fileno()] = sock
+            addresses[sock] = address
+            poll_object.register(sock, select.POLLIN)
+
+        # Incoming data: keep receiving until we see the suffix.
+
+        elif event & select.POLLIN:
+            new_data = sock.recv(4096)
+            if not new_data:  # end-of-file
+                sock.close()  # next poll() will POLLNVAL, and thus clean up
+                continue
+            data = bytes_received.pop(sock, b'') + new_data
+            if data.endswith(b'?'):
+                bytes_to_send[sock] = zen_example.get_answer(data)
+                poll_object.modify(sock, select.POLLOUT)
+            else:
+                bytes_received[sock] = data
+
+        # Socket ready to send: keep sending until all bytes are delivered.
+
+        elif event & select.POLLOUT:
+            data = bytes_to_send.pop(sock)
+            n = sock.send(data)
+            if n < len(data):
+                bytes_to_send[sock] = data[n:]
+            else:
+                poll_object.modify(sock, select.POLLIN)
+
+if __name__ == '__main__':
+    address = zen_example.parse_command_line('simple server')
+    listener = zen_example.create_server_socket(address)
+    serve(listener)
