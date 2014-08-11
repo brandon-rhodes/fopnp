7151a39598182d51b78aa7b90c67e9d9dbc387ea
Brandon Rhodes <brandon@rhodesmill.org>
2014-08-11 10:26:08 -0400

    Fix: remove name of old example protocol

--- py3/chapter07/client.py
+++ py3/chapter07/client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Foundations of Python Network Programming, Third Edition
 # https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter07/client.py
-# Simple Lancelot client that asks three questions then disconnects.
+# Simple Zen-of-Python client that asks three questions then disconnects.
 
 import argparse, random, socket, zen_utils
 
