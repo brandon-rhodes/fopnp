6078381b6b3bded4fe1ec5ad0bdea85928d6eb29
Brandon Rhodes <brandon@rhodesmill.org>
2014-08-26 14:15:52 -0400

    Switch to a pi-based zeromq example

---
+++ py3/chapter08/queuepi.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# Foundations of Python Network Programming, Third Edition
+# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter08/queuecrazy.py
+# Small application that uses several different message queues
+
+import random, threading, time, zmq
+
+def ones_and_zeros(n, d):
+    """Express `n` in at least `d` binary digits, with no special prefix."""
+    return bin(n).lstrip('0b').zfill(d)
+
+def bitsource(zcontext, url):
+    """Produce random points in the unit square."""
+    zsock = zcontext.socket(zmq.PUB)
+    zsock.bind(url)
+    while True:
+        zsock.send_string(ones_and_zeros(random.getrandbits(24), 24))
+        time.sleep(0.01)
+
+def always_yes(zcontext, in_url, out_url):
+    """Coordinates in the lower-left quadrant are inside the unit circle."""
+    isock = zcontext.socket(zmq.SUB)
+    isock.connect(in_url)
+    isock.setsockopt(zmq.SUBSCRIBE, b'00')
+    osock = zcontext.socket(zmq.PUSH)
+    osock.connect(out_url)
+    while True:
+        isock.recv_string()
+        osock.send_string('Y')
+
+def judge(zcontext, in_url, pythagoras_url, out_url):
+    """Determine whether each input coordinate is inside the unit circle."""
+    isock = zcontext.socket(zmq.SUB)
+    isock.connect(in_url)
+    for prefix in b'01', b'10', b'11':
+        isock.setsockopt(zmq.SUBSCRIBE, prefix)
+    psock = zcontext.socket(zmq.REQ)
+    psock.connect(pythagoras_url)
+    osock = zcontext.socket(zmq.PUSH)
+    osock.connect(out_url)
+    while True:
+        bits = isock.recv_string()
+        n, m = int(bits[::2], 2), int(bits[1::2], 2)
+        psock.send_json((n, m))
+        sumsquares = psock.recv_json()
+        osock.send_string('Y' if sumsquares < 2 ** 24 else 'N')
+
+def pythagoras(zcontext, url):
+    """Return the sum-of-squares of number sequences."""
+    zsock = zcontext.socket(zmq.REP)
+    zsock.bind(url)
+    while True:
+        numbers = zsock.recv_json()
+        zsock.send_json(sum(n * n for n in numbers))
+
+def tally(zcontext, url):
+    """Tally how many points fall within the unit circle, and print pi."""
+    zsock = zcontext.socket(zmq.PULL)
+    zsock.bind(url)
+    p = q = 0
+    while True:
+        decision = zsock.recv_string()
+        q += 1
+        if decision == 'Y':
+            p += 4
+        print(decision, p / q)
+
+def start_thread(function, *args):
+    thread = threading.Thread(target=function, args=args)
+    thread.daemon = True  # so you can easily Ctrl-C the whole program
+    thread.start()
+
+def main(zcontext):
+    pubsub = 'tcp://127.0.0.1:6700'
+    reqrep = 'tcp://127.0.0.1:6701'
+    pushpull = 'tcp://127.0.0.1:6702'
+    start_thread(bitsource, zcontext, pubsub)
+    start_thread(always_yes, zcontext, pubsub, pushpull)
+    start_thread(judge, zcontext, pubsub, reqrep, pushpull)
+    start_thread(pythagoras, zcontext, reqrep)
+    start_thread(tally, zcontext, pushpull)
+    time.sleep(30)
+
+if __name__ == '__main__':
+    main(zmq.Context())
