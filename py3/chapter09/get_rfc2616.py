101125e8931d6cf5623d8f3856713215dc62a493
Brandon Rhodes <brandon@rhodesmill.org>
2014-09-23 10:45:30 -0400

    Remove old "get_rfc2616.py" script
    We now do our client code inline in the chapter as doctests (and
    here in the repository as "examples.doctest").

--- py3/chapter09/get_rfc2616.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Foundations of Python Network Programming, Third Edition
-# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter09/get_rfc2616.py
-# Example use of the verbose HTTP request handler.
-
-import urllib, urllib.request, urllib.error, urllib.parse
-from verbose_http import VerboseHTTPHandler
-opener = urllib.request.build_opener(VerboseHTTPHandler)
-opener.open('http://www.ietf.org/rfc/rfc2616.txt')
