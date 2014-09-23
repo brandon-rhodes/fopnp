96accbfae0d973643ba242e26cddc77b64d14515
Brandon Rhodes <brandon@rhodesmill.org>
2014-09-23 11:07:01 -0400

    Bare-bones WSGI app that displays its environment

---
+++ py3/chapter10/wsgi_env.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Foundations of Python Network Programming, Third Edition
+# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter11/wsgi_app.py
+# A simple HTTP service built directly against the low-level WSGI spec.
+
+from pprint import pformat
+from wsgiref.simple_server import make_server
+
+def app(environ, start_response):
+    headers = {'Content-Type': 'text/plain; charset=utf-8'}
+    start_response('200 OK', list(headers.items()))
+    yield 'Here is the WSGI environment:\r\n\r\n'.encode('utf-8')
+    yield pformat(environ).encode('utf-8')
+
+if __name__ == '__main__':
+    httpd = make_server('', 8000, app)
+    host, port = httpd.socket.getsockname()
+    print('Serving on', host, 'port', port)
+    httpd.serve_forever()
