2a70335b032d6a7a746d0c8f9200d71f36cad929
Brandon Rhodes <brandon@rhodesmill.org>
2014-09-25 14:26:15 -0400

    Make the tiny app able to list transactions

--- py3/chapter11/insecure.py
+++ py3/chapter11/insecure.py
@@ -1,5 +1,6 @@
 
-from flask import Flask, make_response, redirect, request, url_for
+import bank
+from flask import Flask, redirect, request, url_for
 app = Flask(__name__)
 
 design = ('<html><head><title>{title}</title>'
@@ -9,15 +10,23 @@ loginform = ('<form method="post"><label>User: <input name="username"></label>'
              '<label>Password: <input name="password" type="password"></label>'
              '<button type="submit">Log in</button></form>')
 mainpage = ('<p>Your transactions:</p><ul>{items}</ul>'
+            '<form method="post" action="/pay">'
+            '<label>To account: <input name="account"></label>'
+            '<label>Amount: <input name="amount"></label>'
+            '<button type="submit">Send money</button>'
+            '</form>'
             '<form method="post" action="/logout">'
-            '<button type="submit">Logout</form>')
+            '<button type="submit">Logout</button></form>')
+payment = '<li>${p.dollars} to account {p.credit}<br>{p.message}'
 
 @app.route('/')
 def index():
     username = request.cookies.get('username')
     if not username:
         return redirect(url_for('login'))
-    return design.format(title='Welcome, ' + username, body=mainpage)
+    payments = bank.get_payments_of(bank.open_database(), '101')
+    body = mainpage.format(items=''.join(payment.format(p=p) for p in payments))
+    return design.format(title='Welcome, ' + username, body=body)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -26,7 +35,6 @@ def login():
     elif request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        print('X', username, password)
         if username == 'brandon' and password == 'atigdng':
             response = redirect(url_for('index'))
             response.set_cookie('username', username)
@@ -37,7 +45,7 @@ def login():
 @app.route('/logout', methods=['POST'])
 def logout():
     response = redirect(url_for('login'))
-    response.set_cookie('username', 'LOGGED-OUT')
+    response.set_cookie('username', '')
     return response
 
 if __name__ == '__main__':
