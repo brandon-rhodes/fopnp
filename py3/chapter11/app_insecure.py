a7e18452ba922fe909243b3335c098ac553bcc73
Brandon Rhodes <brandon@rhodesmill.org>
2014-09-25 22:45:08 -0400

    Move login and logout views to top of module

--- py3/chapter11/app_insecure.py
+++ py3/chapter11/app_insecure.py
@@ -1,8 +1,25 @@
 
-from flask import Flask, redirect, render_template, request, url_for
 import bank
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    username = request.form.get('username', '')
+    password = request.form.get('password', '')
+    if request.method == 'POST':
+        if username == 'brandon' and password == 'atigdng':
+            response = redirect(url_for('index'))
+            response.set_cookie('username', username)
+            return response
+    return render_template('login.html', username=username)
+
+@app.route('/logout')
+def logout():
+    response = redirect(url_for('login'))
+    response.set_cookie('username', '')
+    return response
 
 @app.route('/')
 def index():
@@ -10,9 +27,8 @@ def index():
     if not username:
         return redirect(url_for('login'))
     payments = bank.get_payments_of(bank.open_database(), username)
-    message = request.args.get('message')
-    return render_template('index.html', payments=payments, message=message,
-                           username=username)
+    return render_template('index.html', payments=payments, username=username,
+                           message=request.args.get('message'))
 
 @app.route('/pay', methods=['GET', 'POST'])
 def pay():
@@ -34,23 +50,6 @@ def pay():
         complaint = None
     return render_template('pay.html', complaint=complaint, account=account,
                            dollars=dollars, message=message)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    username = request.form.get('username', '')
-    password = request.form.get('password', '')
-    if request.method == 'POST':
-        if username == 'brandon' and password == 'atigdng':
-            response = redirect(url_for('index'))
-            response.set_cookie('username', username)
-            return response
-    return render_template('login.html', username=username)
-
-@app.route('/logout')
-def logout():
-    response = redirect(url_for('login'))
-    response.set_cookie('username', '')
-    return response
 
 if __name__ == '__main__':
     app.debug = True
