6ac45a1d3ca278cfac9c097a1cbaa45d2ee98bd7
Brandon Rhodes <brandon@rhodesmill.org>
2014-09-29 00:12:18 -0400

    Try radically re-envisioning the scraper

--- py3/chapter11/scraper1.py
+++ py3/chapter11/scraper1.py
@@ -3,30 +3,37 @@ import argparse, requests
 from urllib.parse import urljoin, urlsplit
 from lxml import etree
 
-def visit1(url):
+def GET(url):
     text = requests.get(url).text
     html = etree.HTML(text)
     links = html.findall('.//a[@href]')
-    return [urljoin(url, link.attrib['href']) for link in links]
+    for link in links:
+        yield GET, urljoin(url, link.attrib['href'])
 
-def scrape1(start, visit, url_filter):
-    links_to_visit = [start]
-    already_visited = set()
-    while links_to_visit:
-        url = links_to_visit.pop()
-        print('Visiting', url)
-        more_urls = visit(url)
-        already_visited.add(url)
-        new_urls = set(filter(url_filter, more_urls)) - already_visited
-        links_to_visit.extend(new_urls)
+def scrape(start, url_filter):
+    calls_to_make = [start]
+    calls_already_made = set()
+    while calls_to_make:
+        call_info = calls_to_make.pop()
+        function, url, *etc = call_info
+        print(function.__name__, url, *etc)
+        more_calls_to_make = function(url, *etc)
+        calls_already_made.add(call_info)
+        for call_info in more_calls_to_make:
+            if call_info in calls_already_made:
+                continue
+            function, url, *etc = call_info
+            if not url_filter(url):
+                continue
+            calls_to_make.append(call_info)
 
-def main(scrape, visit):
+def main(GET):
     parser = argparse.ArgumentParser(description='Scrape a simple site.')
     parser.add_argument('url', help='the URL at which to begin')
-    start = parser.parse_args().url
-    starting_netloc = urlsplit(start).netloc
+    start_url = parser.parse_args().url
+    starting_netloc = urlsplit(start_url).netloc
     url_filter = (lambda url: urlsplit(url).netloc == starting_netloc)
-    scrape(start, visit, url_filter)
+    scrape((GET, start_url), url_filter)
 
 if __name__ == '__main__':
-    main(scrape1, visit1)
+    main(GET)
