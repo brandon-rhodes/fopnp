cc8cdfc114655f2b874ab4ee7ce00db451135f41
Brandon Rhodes <brandon@rhodesmill.org>
2014-09-29 00:30:00 -0400

    Get Ghost.py scraper kind of working!

--- py3/chapter11/scraper2.py
+++ py3/chapter11/scraper2.py
@@ -9,17 +9,29 @@ class GhostVisitor:
         self.ghost = Ghost()
 
     def GET(self, url):
-        page, extra_resources = self.ghost.open(url)
+        page, resources = self.ghost.open(url)
         if page.http_status != 200:
             print('Status', page.http_status, 'for', url)
             return
 
+        yield from self.parse(page)
+
+        js = 'document.getElementsByTagName("form").length'
+        form_count, resources = self.ghost.evaluate(js)
+        if form_count:
+            yield self.submit_form, url
+
+    def parse(self, page):
         html = etree.HTML(self.ghost.content)
         links = html.findall('.//a[@href]')
         for link in links:
-            yield self.GET, urljoin(url, link.attrib['href'])
+            yield self.GET, urljoin(page.url, link.attrib['href'])
 
-        
+    def submit_form(self, url):
+        page, resources = self.ghost.open(url)
+        page, resources = self.ghost.fire_on("form", "submit",
+                                             expect_loading=True)
+        yield from self.parse(page)
 
 if __name__ == '__main__':
     main(GhostVisitor().GET)
