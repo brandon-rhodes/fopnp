c92077526c98ccd35dc8346e59b73b658cc984c1
Brandon Rhodes <brandon@rhodesmill.org>
2014-10-21 14:04:11 -0400

    Add script at old URL that is mentioned in book

---
+++ py3/chapter08/queuecrazy.py
@@ -0,0 +1,8 @@
+# The script printed in the book that calls itself "queuecrazy.py" is
+# actually named "queuepi.py", as you can see a page or two later when
+# the book demonstrates how to invoke the script at the shell prompt.
+# So the script actually lives at the URL:
+
+# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter08/queuepi.py
+
+# Sorry about that!
