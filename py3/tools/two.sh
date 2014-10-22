04d41c42320e4ae5e4cbf6dd5139d20410f8d245
Brandon Rhodes <brandon@rhodesmill.org>
2014-10-22 11:07:41 -0400

    Have "two.sh" rewrite "python3" to "python2"

--- py3/tools/two.sh
+++ py3/tools/two.sh
@@ -12,6 +12,7 @@ do
     two=$(echo $chapter | sed 's/chapter/two/')
     rm -rf $two
     cp -r $chapter $two
+    sed 's/\$ python3 /\$ python2 /' $chapter/README.md > $two/README.md
     3to2 -w $two/*.py
     tools/run.sh $two/README.md
     diff -u $chapter/README.md $two/README.md > $two/README.diff
