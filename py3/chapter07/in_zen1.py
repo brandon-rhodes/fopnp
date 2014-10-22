a49bd8ea97cc1022bf9c4be7e51241272c1d719c
Brandon Rhodes <brandon@rhodesmill.org>
2014-10-22 14:32:17 -0400

    Use a /tmp log file, to assure we can write to it
    The idea of writing to the current working directory is a bad one,
    because we cannot easily predict what that working directory will be.
    (And if it is /, as appears with this version of inetd, then we cannot
    create files there at all.)

--- py3/chapter07/in_zen1.py
+++ py3/chapter07/in_zen1.py
@@ -8,7 +8,7 @@ import socket, sys, zen_utils
 if __name__ == '__main__':
     sock = socket.fromfd(0, socket.AF_INET, socket.SOCK_STREAM)
     sys.stdin = open('/dev/null', 'r')
-    sys.stdout = sys.stderr = open('log.txt', 'a', buffering=1)
+    sys.stdout = sys.stderr = open('/tmp/zen.log', 'a', buffering=1)
     address = sock.getpeername()
     print('Accepted connection from {}'.format(address))
     zen_utils.handle_conversation(sock, address)
--- py3/chapter07/in_zen2.py
+++ py3/chapter07/in_zen2.py
@@ -8,7 +8,7 @@ import socket, sys, zen_utils
 if __name__ == '__main__':
     listener = socket.fromfd(0, socket.AF_INET, socket.SOCK_STREAM)
     sys.stdin = open('/dev/null', 'r')
-    sys.stdout = sys.stderr = open('log.txt', 'a', buffering=1)
+    sys.stdout = sys.stderr = open('/tmp/zen.log', 'a', buffering=1)
     listener.settimeout(8.0)
     try:
         zen_utils.accept_connections_forever(listener)
