cc1bb14e2883b2fd4e9b4a8a31994bad723efdbd
Brandon Rhodes <brandon@rhodesmill.org>
2014-10-23 14:20:23 -0400

    Replace terrible "play.sh" with vastly better one
    Now that we keep the hosts h1 through h4 up and running persistently, we
    can use the wonderful new "docker exec" command to enter one of them
    without having to create it fresh every time, which was both causing
    hassle and also failing about half of the time.

--- playground/play.sh
+++ playground/play.sh
@@ -1,76 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
-set -e
+if [ -z "$1" -o -n "$2" ]
+then
+    cat <<'EOF'
+usage: play.sh HOST
 
-if [ "$#" = "0" ]
-then
-    container=h1
-else
-    container=$1
-    shift
-fi
-if [ "$#" = "0" ]
-then
-    set /bin/bash
-fi
-ppid=$$
+Hosts available in the playground:
+    h1, h2, h3      Hosts behind broadband modemA
+    h4              Host behind broadband modemB
+    modemA, modemB  Broadband modems doing NAT
+    isp, backbone   Internet routers
+    example         The gateway machine of example.com
+    ftp, mail, www  Servers behind the example.com gateway
 
-if [ ! -d /proc/sys/net/ipv4/conf/playhome ]
-then
-    echo 'Error: "network.sh" is not yet up and running' >&2
-    exit 1
-fi
+               backbone
+              /        \
+           isp          example
+          /   \        /   |   \
+     modemA   modemB  ftp mail www
+    /  |  \     |
+    h1 h2 h3    h4
 
-if [ -n "${container/h[1-9]/}" ]
-then
-    echo 'Error: hostname should be between "h1" and "h9"' >&2
-    exit 1
+EOF
+    exit
 fi
 
-n=${container/h/}
-
-setup () {
-
-    # At the bottom of this script, our parent process uses "exec" to
-    # replace itself with an interactive "docker" whose networking we
-    # need to configure.  So, as long as our parent process is still
-    # running, we watch for the container to appear.
-
-    pid=""
-    while [ -z "$pid" ]
-    do
-        if ! ps -p $ppid >/dev/null
-        then
-            echo "play.sh: giving up and exiting, because container died"
-            exit 1
-        fi
-        sleep .25
-        pid=$(docker inspect -f '{{.State.Pid}}' $container 2>/dev/null ||true)
-    done
-
-    # The container now exists, so we can configure its networking.
-
-    if ip link show $container-peer >/dev/null 2>&1
-    then
-        # Clean up after previous attempt to plumb this container.
-        sudo ip link del $container-peer
-    fi
-    sudo rm -f /var/run/netns/$pid
-    sudo ln -s /proc/$pid/ns/net /var/run/netns/$pid
-    sudo ip link add $container-eth0 type veth peer name $container-peer
-    sudo ip link set $container-peer netns $pid
-    sudo ip netns exec $pid ip link set dev $container-peer name eth0
-    sudo ip netns exec $pid ip link set dev eth0 up
-    sudo ip netns exec $pid ip addr add 192.168.1.1$n/24 dev eth0
-    sudo ip netns exec $pid ip route add default via 192.168.1.1
-    sudo brctl addif playhome $container-eth0
-    sudo ip link set dev $container-eth0 up
-    sudo rm /var/run/netns/$pid
-}
-
-py3=$(readlink -f ../py3)
-sudo true  # make user type password before "setup" goes into background
-setup &
-exec docker run --name=$container --hostname=$container --privileged=true \
-     --net=none --dns=10.1.1.1 --dns-search=example.com \
-     --volume=$py3:/py3 --rm -ti fopnp/base "$@"
+exec docker exec -it $1 /bin/bash
