1354a793f3480a5ecf1e092f8005cfa2534eeeab
Brandon Rhodes <brandon@rhodesmill.org>
2014-10-23 12:48:25 -0400

    Plumb the two home network LANs correctly

--- playground/launch.sh
+++ playground/launch.sh
@@ -62,30 +62,68 @@ bridge_add_interface () {
 # Build the playground.
 
 start_container h1 fopnp/base
-# start h2 fopnp/base
-# start h3 fopnp/base
-# start h4 fopnp/base
+start_container h2 fopnp/base
+start_container h3 fopnp/base
+start_container h4 fopnp/base
+start_container modemA fopnp/base
+start_container modemB fopnp/base
+start_container isp fopnp/base
+start_container backbone fopnp/dns
 start_container example.com fopnp/base
 start_container ftp.example.com fopnp/ftp
 start_container mail.example.com fopnp/mail
 start_container www.example.com fopnp/www
 
+# For each LAN, create an ethernet bridge and corresponding interfaces.
+
+create_interface h1-eth0
+create_interface h2-eth0
+create_interface h3-eth0
+create_interface h4-eth0
+create_interface modemA-eth1
+create_interface modemB-eth1
+
 start_bridge homeA
+
+bridge_add_interface homeA modemA-eth1
+bridge_add_interface homeA h1-eth0
+bridge_add_interface homeA h2-eth0
+bridge_add_interface homeA h3-eth0
+
 start_bridge homeB
-start_bridge exampleCOM
+
+bridge_add_interface homeB modemB-eth1
+bridge_add_interface homeB h1-eth0
 
 create_interface example-eth1
 create_interface ftp-eth0
 create_interface mail-eth0
 create_interface www-eth0
 
-#create_interface_pair example-eth1 example-peer
-# create_interface_pair www-eth0 www-peer
+start_bridge exampleCOM
 
 bridge_add_interface exampleCOM example-eth1
 bridge_add_interface exampleCOM ftp-eth0
 bridge_add_interface exampleCOM mail-eth0
 bridge_add_interface exampleCOM www-eth0
+
+# Configure the LAN behind each broadband modem.
+
+for modem in modemA modemB
+do
+    # sudo ip netns exec $modem ip addr add 10.25.1.65/16 dev eth0
+    sudo ip netns exec $modem ip addr add 192.168.1.1/24 dev eth1
+    # sudo ip netns exec $modem ip route add default via 10.25.1.1
+    # sudo ip netns exec $modem iptables --table nat \
+    #     --append POSTROUTING --out-interface eth0 -j MASQUERADE
+done
+
+for host in h1 h2 h3 h4
+do
+    n=${host#?}
+    sudo ip netns exec $host ip addr add 192.168.1.1$n/24 dev eth0
+    sudo ip netns exec $host ip route add default via 192.168.1.1
+done
 
 # Configure the 10.130.1.* network that the example.com machines share.
 
