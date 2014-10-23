cb6d88518cee054beadc3b500ce1ebaee5243705
Brandon Rhodes <brandon@rhodesmill.org>
2014-10-23 11:03:02 -0400

    Switch to all Docker images using the same CMD

--- playground/base/startup.sh
+++ playground/base/startup.sh
@@ -5,5 +5,10 @@
 # exists in the networking playground will append additional commands to
 # this startup script using RUN commands in their Dockerfiles.
 
+if tail -1 "$0" | grep -q /etc/init.d/ssh
+then
+    ssh_options="-D"  # if no commands follow "ssh", run it in foreground
+fi
+
 /etc/init.d/rsyslog start
-/etc/init.d/ssh start
+/etc/init.d/ssh start $ssh_options
