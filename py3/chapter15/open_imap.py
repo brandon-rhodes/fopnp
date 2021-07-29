1259462967210421ed93c5946e4b831e6484e701
rstewart2702 <rstewart2702@gmail.com>
2021-07-29 17:42:43 -0500

    This change also works (PEP 461 was a helpful read...)

--- py3/chapter15/open_imap.py
+++ py3/chapter15/open_imap.py
@@ -22,10 +22,7 @@ def main():
         print('Listing mailboxes:')
         data = c.list_folders()
         for flags, delimiter, folder_name in data:
-            print('  %-30s%s %s' %
-                    ( ( b' '.join(flags) ).decode() ,
-                      delimiter.decode() ,
-                      folder_name ) )
+            print('  %-30s%s %s' % ( b' '.join(flags), delimiter, folder_name ) )
     finally:
         c.logout()
 
